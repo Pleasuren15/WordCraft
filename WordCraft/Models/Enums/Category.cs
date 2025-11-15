0e2b0a8e50fb3f26724d45e035d3d022cc19afd6
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-11-15 13:34:32 +0200

    wip: Alphabet & category added

---
+++ WordCraft/Models/Enums/Category.cs
@@ -0,0 +1,20 @@
+﻿namespace WordCraft.Models.Enums;
+
+public enum Category
+{
+    All = 0,
+    Wordle = 1,
+    BrainRot = 2,
+    Countries = 3,
+    CapitalOfCountries = 4,
+    Sports = 5,
+    Animals = 6,
+    Birds = 7,
+    Softwares = 8,
+    ProgrammingLanguages = 9,
+    Games = 10,
+    PcGames = 11,
+    MobileGames = 12,
+    Consolegames = 13,
+    Companies = 14
+}
